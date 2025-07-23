5a4a34d6001d1ec8a69dff310c101a1880cd8181
carlos <Casanbre@gmail.com>
2025-07-23 09:27:43 -0500

    AJUSTE DE SELECT

--- public/Maquina_4/logic.js
+++ public/Maquina_4/logic.js
@@ -1,34 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("formulario")
-    .addEventListener("submit", async (e) => {
-      e.preventDefault();
-
-      const datos = {
-        SUPERVISOR: document.getElementById("SUPERVISOR").value,
-        CANTIDAD: document.getElementById("CANTIDAD").value,
-        FECHA_INCIAL: document.getElementById("FECHA_INCIAL").value,
-        FECHA_FINAL: document.getElementById("FECHA_FINAL").value,
-        TIEMPO_PRODUCCION: document.getElementById("TIEMPO_PRODUCCION").value,
-        TIEMPO_PARADA: document.getElementById("TIEMPO_PARADA").value,
-      };
-
-      try {
-        const res = await fetch("/api/maquinas", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(datos),
-        });
-
-        if (res.ok) {
-          alert("Datos guardados correctamente ✅");
-          document.getElementById("formulario").reset(); // limpia campos
-        } else {
-          alert("Error al guardar los datos ❌");
+    document
+      .getElementById("formulario")
+      .addEventListener("submit", async (e) => {
+        e.preventDefault();
+  
+        const datos = {
+          SUPERVISOR: document.getElementById("nombreSupervisor").value,
+          CANTIDAD: document.getElementById("estanteriasTrefiladas").value,
+          FECHA_INCIAL: document.getElementById("fechaInicioTurno").value,
+          FECHA_FINAL: document.getElementById("fechaFinalTurno").value,
+          TIEMPO_PRODUCCION: document.getElementById("tiempoProduccion").value,
+          TIEMPO_PARADA: document.getElementById("tiempoParadas").value,
+        };
+  
+        try {
+          const res = await fetch("/api/maquinas", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(datos),
+          });
+  
+          if (res.ok) {
+            alert("Datos guardados correctamente ✅");
+            document.getElementById("formulario").reset(); // limpia campos
+          } else {
+            alert("Error al guardar los datos ❌");
+          }
+        } catch (error) {
+          console.error("Error de red:", error);
+          alert("Fallo la conexión con el servidor ❌");
         }
-      } catch (error) {
-        console.error("Error de red:", error);
-        alert("Fallo la conexión con el servidor ❌");
-      }
-    });
-});
+      });
+  });
+  
